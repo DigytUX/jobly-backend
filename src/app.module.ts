5e2f2266619a0b7b126b712544dc4fb3a4e01c6e
Chris <chrisalexander0617@gmail.com>
2023-05-05 14:34:38 -0400

    fixed bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { ContactsModule } from './jobs/contacts/contacts.module';
+import { ContactsModule } from './contacts/contacts.module';
 import { JobsModule } from './jobs/jobs.module';
 import { TechnologiesModule } from './technologies/technologies.module';
 
